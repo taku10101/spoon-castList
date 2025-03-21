c89db211a9fe39c4453825fd7a4e8a443bee25bd
taku10101 <taku1011ht@gmail.com>
2025-03-21 23:28:06 +0900

    add: select  component

---
+++ src/components/UserSelect.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { userList } from "@/data/userList";
+
+export const UserSelect = () => {
+  const handleSelectChange = (userId: string) => {
+    window.history.pushState(null, "", `?userId=${userId}`);
+  };
+  const users = userList;
+  return (
+    <Select onValueChange={handleSelectChange}>
+      <SelectTrigger className='w-[180px]'>
+        <SelectValue placeholder='Select a user' />
+      </SelectTrigger>
+      <SelectContent>
+        {users.map((user) => (
+          <SelectItem key={user.userId} value={user.userId}>
+            {user.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
