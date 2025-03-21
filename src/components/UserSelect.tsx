8533ec6cc8ff682455b21f3e38af8631834c82d6
taku10101 <taku1011ht@gmail.com>
2025-03-22 00:06:07 +0900

    fix select

--- src/components/UserSelect.tsx
+++ src/components/UserSelect.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -10,23 +10,29 @@ import {
 import { userList } from "@/data/userList";
 
 export const UserSelect = () => {
+  const [selectedUserId, setSelectedUserId] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const userIdFromUrl = new URLSearchParams(window.location.search).get(
+      "userId"
+    );
+    setSelectedUserId(userIdFromUrl || userList[0]?.userId);
+  }, []);
+
   const handleSelectChange = (userId: string) => {
     window.history.pushState(null, "", `?userId=${userId}`);
     window.location.reload();
   };
 
-  const users = userList;
-  const userId =
-    new URLSearchParams(window.location.search).get("userId") ||
-    users[0]?.userId;
-
   return (
-    <Select onValueChange={handleSelectChange} defaultValue={userId}>
+    <Select onValueChange={handleSelectChange} defaultValue={selectedUserId}>
       <SelectTrigger className='w-[280px]'>
         <SelectValue placeholder='Select a user' />
       </SelectTrigger>
       <SelectContent>
-        {users.map((user) => (
+        {userList.map((user) => (
           <SelectItem key={user.userId} value={user.userId}>
             <div>
               <span>{user.name}</span>
